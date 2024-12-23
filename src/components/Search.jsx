4ad11d80062b2bb06d6de8d4e76a3a5006b129c8
insert github real name here <insert github email here>
2024-12-23 14:33:10 -0800

    first commit

---
+++ src/components/Search.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import styles from "./search.module.css";
+
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = "731e15c1e9404e9385c24458839c07bf";
+
+export default function Search({ foodData, setFoodData }) {
+  const [query, setQuery] = useState("pizza");
+  //Syntax of the useffect hook
+  useEffect(() => {
+    async function fetchFood() {
+      const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data.results);
+      setFoodData(data.results);
+    }
+    fetchFood();
+  }, [query]);
+  return (
+    <div className={styles.searchContainer}>
+      <input
+        className={styles.input}
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        type="text"
+      />
+    </div>
+  );
+}
